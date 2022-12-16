83f3e743c51b71bb033f8548af585b8720bc7280
Richard Chukwuma <45286636+Reechychukz@users.noreply.github.com>
2022-12-16 01:29:09 +0100

    Remove ignored files

---
+++ src/Infrastructure.Data/Migrations/20220926154209_friend-mgmt.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    public partial class friendmgmt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserFriendship");
+
+            migrationBuilder.CreateTable(
+                name: "UserFriend",
+                columns: table => new
+                {
+                    RequestedById = table.Column<Guid>(type: "uuid", nullable: false),
+                    RequestedToId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserFriendId = table.Column<Guid>(type: "uuid", nullable: false),
+                    RequestTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    BecameFriendsTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    FriendRequestFlag = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    CreatedById = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserFriend", x => new { x.RequestedById, x.RequestedToId });
+                    table.ForeignKey(
+                        name: "FK_UserFriend_Users_RequestedById",
+                        column: x => x.RequestedById,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserFriend_Users_RequestedToId",
+                        column: x => x.RequestedToId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserFriend_RequestedToId",
+                table: "UserFriend",
+                column: "RequestedToId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserFriend");
+
+            migrationBuilder.CreateTable(
+                name: "UserFriendship",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserFriendId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserFriendship", x => new { x.UserId, x.UserFriendId });
+                    table.ForeignKey(
+                        name: "FK_UserFriendship_Users_UserFriendId",
+                        column: x => x.UserFriendId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_UserFriendship_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserFriendship_UserFriendId",
+                table: "UserFriendship",
+                column: "UserFriendId");
+        }
+    }
+}
